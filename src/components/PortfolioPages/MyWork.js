c98e0ce905922b7cbfbb446160739e6e5a972a28
Justin Smith <jj.smith32199@gmail.com>
2023-05-01 19:29:58 -0400

    started MyWork.js file for displaying projects

---
+++ src/components/PortfolioPages/MyWork.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export default function MyWork() {
+    return(
+    <section id="projects">
+        <h3> My Work </h3>
+        <a href="https://jjsmith32199.github.io/challenge1/#social-media-marketing">
+          <img title="Horiseon Landing Page With Accessability Focus" src="./assets/images/digital-marketing-meeting.jpg" class="project1" alt="Horiseon Landing Page Image"/>
+        </a>
+        <a href="https://github.com/jjsmith32199/UntitledGooseProject">
+          <img src="./assets/images/Cinevents.png" class="project2" alt="CinEvents logo"/>
+        </a>
+        <a href="https://github.com/jjsmith32199/HealthNoteTracker">
+          <img src="./assets/images/HealthNoteTracker.png" class="project3" alt="Health Tracker Landing Page"/>
+        </a>
+        <a href="https://github.com/jjsmith32199/WhosThatEmployee" class/>
+    </section>
+    )
+}
